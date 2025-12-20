618e679efa6ebad7b624dfd16e80a20a6b40f66e
Arief Dwi Septian <ariefdwis13@gmail.com>
2025-12-20 16:47:28 +0700

    Update modelling.py ke versi DagsHub agar terhubung ke Remote MLflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,83 +1,53 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import mlflow
-import dagshub
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-
-
-
-DAGSHUB_USERNAME = "Ariefdwis"  
-DAGSHUB_REPO_NAME = "Eksperimen_MSML_AriefDwiSeptian_pt2" 
+from sklearn.preprocessing import LabelEncoder
+import mlflow
+import mlflow.sklearn
+import dagshub
+import os
 
 def main():
-    print("Memulai proses Training Model ")
 
+    dagshub_token = os.environ.get("DAGSHUB_USER_TOKEN")
+    if dagshub_token:
+        print("Authenticating with DagsHub Token...")
+        dagshub.auth.add_app_token(dagshub_token)
     
-    dagshub.init(repo_owner=DAGSHUB_USERNAME, repo_name=DAGSHUB_REPO_NAME, mlflow=True)
-    mlflow.set_experiment("Eksperimen Water Potability")
 
+    dagshub.init(repo_owner='Ariefdwis', repo_name='Eksperimen_MSML_AriefDwiSeptian_pt2', mlflow=True)
     
-    try:
-        df = pd.read_csv('water_potability_clean.csv')
-    except FileNotFoundError:
-        print("File 'water_potability_clean.csv' tidak ditemukan di folder ini!")
-        return
+
+    mlflow.set_tracking_uri("https://dagshub.com/Ariefdwis/Eksperimen_MSML_AriefDwiSeptian_pt2.mlflow")
+    mlflow.set_experiment("CI_CD_Retraining_Experiment")
+
+    print("MLflow Tracking URI:", mlflow.get_tracking_uri())
+
+
+    df = pd.read_csv('water_potability_clean.csv')
+
+    df.fillna(df.mean(), inplace=True) 
 
     X = df.drop('Potability', axis=1)
     y = df['Potability']
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    
+
+    mlflow.autolog()
+
     with mlflow.start_run():
-        print(" Sedang melakukan Hyperparameter Tuning...")
+        print("Mulai Training Model di CI/CD...")
+        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        model.fit(X_train, y_train)
         
+        accuracy = model.score(X_test, y_test)
+        print(f"Model Accuracy: {accuracy}")
         
-        rf = RandomForestClassifier(random_state=42)
-        param_grid = {
-            'n_estimators': [50, 100],
-            'max_depth': [None, 10, 20],
-            'min_samples_split': [2, 5]
-        }
-        
-     
-        grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=1, verbose=1)
-        grid_search.fit(X_train, y_train)
+   
+        mlflow.log_metric("accuracy_manual", accuracy)
 
-        best_model = grid_search.best_estimator_
-        best_params = grid_search.best_params_
-        
-        
-        y_pred = best_model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-        print(f"Akurasi Terbaik: {acc:.4f}")
-        print(f"Parameter Terbaik: {best_params}")
-
-        mlflow.log_params(best_params)
-        mlflow.log_metric("accuracy", acc)
-
-        
-        mlflow.sklearn.log_model(best_model, "model_random_forest")
-
-        
-        plt.figure(figsize=(6,5))
-        cm = confusion_matrix(y_test, y_pred)
-        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-        plt.title('Confusion Matrix')
-        plt.ylabel('Aktual')
-        plt.xlabel('Prediksi')
-        plt.savefig("confusion_matrix.png")
-        mlflow.log_artifact("confusion_matrix.png") 
-
-        
-        report = classification_report(y_test, y_pred)
-        with open("classification_report.txt", "w") as f:
-            f.write(report)
-        mlflow.log_artifact("classification_report.txt") 
-        
-        print("Selesai! Model dan Artefak telah terkirim ke DagsHub.")
+    print("Training Selesai! Data terkirim ke DagsHub.")
 
 if __name__ == "__main__":
     main()
